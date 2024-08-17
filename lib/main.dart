34c9a60408714db2267d265d5d7402b00c2c6d9f
USER <afsanamim5220@gamil.com>
2024-08-17 21:20:00 +0600

    rhdnf

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(), // Wrap your app
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +15,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeActivity());
+    return MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        home: HomeActivity());
   }
 }
 
@@ -36,54 +47,66 @@ class HomeActivity extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: OrientationBuilder(builder: (context, Orientation orientation) {
-        print(orientation);
-        if(orientation==Orientation.portrait){
-          return Column(
-            children: [
-              Wrap(
-                  alignment: WrapAlignment.center,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 10,
-                  children: [
-                    Text('hdsfhd '),
-                    Text('hdsfhd fjdhf dfdh'),
-                    Text('hdsfhd fjdhf dfdhjdfd'),
-                    Text('random'),
-                    Text('hdsfhd fjdhf dfdhjdfd'),
-                  ]),
-              Text(orientation.name)
-            ],
-          );
-        }else {
-        return  Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Hello'),
-              Text('hi'),
-              Text(orientation.name)
-            ],
-          ),
-        );
-        }
-        // return Column(
-        //   children: [
-        //     Wrap(
-        //         alignment: WrapAlignment.center,
-        //         crossAxisAlignment: WrapCrossAlignment.center,
-        //         spacing: 10,
-        //         children: [
-        //           Text('hdsfhd '),
-        //           Text('hdsfhd fjdhf dfdh'),
-        //           Text('hdsfhd fjdhf dfdhjdfd'),
-        //           Text('random'),
-        //           Text('hdsfhd fjdhf dfdhjdfd'),
-        //         ]),
-        //     Text(orientation.name)
-        //   ],
-        // );
-      }),
+      // body: OrientationBuilder(builder: (context, Orientation orientation) {
+      //   print(orientation);
+      //   if(orientation==Orientation.portrait){
+      //     return Column(
+      //       children: [
+      //         Wrap(
+      //             alignment: WrapAlignment.center,
+      //             crossAxisAlignment: WrapCrossAlignment.center,
+      //             spacing: 10,
+      //             children: [
+      //               Text('hdsfhd '),
+      //               Text('hdsfhd fjdhf dfdh'),
+      //               Text('hdsfhd fjdhf dfdhjdfd'),
+      //               Text('random'),
+      //               Text('hdsfhd fjdhf dfdhjdfd'),
+      //             ]),
+      //         Text(orientation.name)
+      //       ],
+      //     );
+      //   }else {
+      //   return  Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.start,
+      //       children: [
+      //         Text('Hello'),
+      //         Text('hi'),
+      //         Text(orientation.name)
+      //       ],
+      //     ),
+      //   );
+      //   }
+      //   // return Column(
+      //   //   children: [
+      //   //     Wrap(
+      //   //         alignment: WrapAlignment.center,
+      //   //         crossAxisAlignment: WrapCrossAlignment.center,
+      //   //         spacing: 10,
+      //   //         children: [
+      //   //           Text('hdsfhd '),
+      //   //           Text('hdsfhd fjdhf dfdh'),
+      //   //           Text('hdsfhd fjdhf dfdhjdfd'),
+      //   //           Text('random'),
+      //   //           Text('hdsfhd fjdhf dfdhjdfd'),
+      //   //         ]),
+      //   //     Text(orientation.name)
+      //   //   ],
+      //   // );
+      // }),
+      body: LayoutBuilder(
+        builder: (context,BoxConstraints constraints){
+          if (constraints.maxWidth < 640) {
+            return Text('this is a PHONE');
+          } else if (constraints.maxWidth > 640 &&
+              constraints.maxWidth < 1008) {
+            return Text(' this is table');
+          } else {
+            return Text('LAPTOP/DESKTOP');
+          }
+        },
+      ),
     );
-  }
-}
+  }}
+
